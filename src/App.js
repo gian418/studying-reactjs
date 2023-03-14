d34d4a71475a51dbd9d1cd856f9d7558953494de
Giancarlo Haack Albano <gian418@gmail.com>
2023-03-14 20:33:06 -0300

    fragments e mais um pouco sobre props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
 
@@ -24,6 +25,8 @@ function App() {
               idade="28" 
               profissao="Programador"
               foto="https://via.placeholder.com/150" />
+
+      <List />
 
     </div>
   )
