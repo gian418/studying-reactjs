5991251b8a5eacd9cd8da3e48b5b28288fb6514c
Giancarlo Haack Albano <gian418@gmail.com>
2023-03-14 07:24:16 -0300

    props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const name = 'Gian'
+
+  const nome = "Maria"
 
   return (
     <div className="App">
-      <h1>Alterando o JSX</h1>
-      <p>Olá, {name}</p>
-      
-      <HelloWorld />
-    
-    </div>
+      <SayMyName nome="Gian" />
+      <SayMyName nome="Haack" />
+      <SayMyName nome={nome} />
 
-  );
+      <Pessoa nome="Rodrigo" 
+              idade="28" 
+              profissao="Programador"
+              foto="https://via.placeholder.com/150" />
+
+    </div>
+  )
 }
 
 export default App;
