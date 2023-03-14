e4fa4a8996c0d090c266e66b06e0af384f494924
Giancarlo Haack Albano <gian418@gmail.com>
2023-03-14 20:56:43 -0300

    Eventos

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
+
 
 function App() {
 
-  const nome = "Maria"
-
   return (
     <div className="App">
+      <h1>Testando Eventos</h1>
+      <Evento numero="1"/>
+      <Evento numero="2"/>
 
-      <Frase />
-      <Frase />
-
-
-
-      <SayMyName nome="Gian" />
-      <SayMyName nome="Haack" />
-      <SayMyName nome={nome} />
-
-      <Pessoa nome="Rodrigo" 
-              idade="28" 
-              profissao="Programador"
-              foto="https://via.placeholder.com/150" />
-
-      <List />
-
+      <Form />
     </div>
   )
 }
